66cd6024342e13e9c696c4057e374b4ed918cb78
Renan Luiz Vendramini <rlvendramini@outlook.com>
2022-03-28 16:06:25 -0300

    Initial Commit

---
+++ lib/mercosur_plate_converter.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "mercosur_plate_converter/version"
+
+module MercosurPlateConverter
+  class Error < StandardError; end
+  # Your code goes here...
+end
